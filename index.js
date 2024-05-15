b46c87ce092a6ea33d7231f897b8c28faa66def3
34x340 <114092241+34x340@users.noreply.github.com>
2024-05-15 18:30:37 +0300

    dz

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const imgs = document.querySelectorAll("img")
+// 1
+const imgs = document.querySelectorAll('img[data-src]');
 const onEntry = (entries, observer) => {
-    console.log(observer)
     entries.forEach(entry => {
-        entry.loading = 'lazyload'
+        if (entry.isIntersecting) {
+            entry.target.src = entry.target.dataset.src;
+            entry.target.classList.add('load')
+            entry.target.removeAttribute('data-src');
+            observer.unobserve(entry.target);
+        }
     });
 }
 const observer = new IntersectionObserver(onEntry, {
-    rootMargin: "50px",
+    rootMargin: '50px',
     threshold: 0.5,
-})
+});
+imgs.forEach(img => observer.observe(img));
+// 2
+const imgsTwo = document.querySelectorAll('img[data-srcTwo]');
 
-imgs.forEach(img => observer.observe(img))
+imgsTwo.forEach(imgTwo => imgTwo.addEventListener('click', (e) => {
+    e.target.src = e.target.dataset.srctwo;
+    e.target.style.width = 'auto';
+    e.target.style.backgroundColor = 'transparent';
+    e.target.classList.add('load');
+    e.target.addEventListener('transitionend', () => {
+        e.target.classList.remove('load');
+    }, {once: true});
+}))
